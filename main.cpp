080db9652c8fa90a7bbf498558efddd05682ccb0
Turbo <1129216137@qq.com>
2021-11-17 18:01:38 +0800

    测试

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 extern "C"
 {
 #include "libavformat/avformat.h"
+#include "libavcodec/avcodec.h"
 }
 #pragma comment(lib,"E:/FFMPEGTest/TurboPlayer/thirdParty/ffmpeg3.4.2/lib/win64/avformat.lib")
 #pragma comment(lib,"E:/FFMPEGTest/TurboPlayer/thirdParty/ffmpeg3.4.2/lib/win64/avutil.lib")
@@ -15,6 +16,10 @@ int main(int argc, char *argv[])
     av_register_all();
     // 初始化网络库
     avformat_network_init();
+
+    // 注册解码器
+    avcodec_register_all();
+
     AVDictionary *opts = nullptr;
     // 设置rtsp参数
     av_dict_set(&opts,"rtsp_transport","tcp",0);
@@ -65,6 +70,54 @@ int main(int argc, char *argv[])
     //获取视频流
     av_find_best_stream(ic,AVMEDIA_TYPE_VIDEO,-1,-1, nullptr,0);
 
+    //找到解码器
+    AVCodec *vCodec = avcodec_find_decoder(ic->streams[0]->codecpar->codec_id);
+
+    // 创建爱解码器上下文
+    AVCodecContext *vc = avcodec_alloc_context3(vCodec);
+
+    // 配置解码器上下文
+    avcodec_parameters_to_context(vc,ic->streams[0]->codecpar);
+
+    //八线程解码器
+    vc->thread_count = 8;
+
+    ret = avcodec_open2(vc,0,0);
+
+    if(ret!=0)
+    {
+        char buf[1024]={0};
+        av_strerror(ret,buf,sizeof(buf)-1);
+        std::cout<< buf <<std::endl;
+        getchar();
+        return -1;
+    }
+
+    // 音频解码器
+    //找到解码器
+    AVCodec *aCodec = avcodec_find_decoder(ic->streams[1]->codecpar->codec_id);
+
+    // 创建爱解码器上下文
+    AVCodecContext *ac = avcodec_alloc_context3(aCodec);
+
+    // 配置解码器上下文
+    avcodec_parameters_to_context(ac,ic->streams[1]->codecpar);
+
+    //八线程解码器
+    ac->thread_count = 8;
+
+    ret = avcodec_open2(ac,0,0);
+
+    if(ret!=0)
+    {
+        char buf[1024]={0};
+        av_strerror(ret,buf,sizeof(buf)-1);
+        std::cout<< buf <<std::endl;
+        getchar();
+        return -1;
+    }
+
+
     AVPacket *pkt = av_packet_alloc();
     while(true)
     {
@@ -81,6 +134,7 @@ int main(int argc, char *argv[])
 
         // 释放，引用计数为-1时候释放控件
         av_packet_unref(pkt);
+        break;
     }
     av_packet_free(&pkt);
     if(ic)
